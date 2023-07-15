06e304c4c6f6f1d6e830f90ef5799a995b12ce42
jonathan-robertson <jrob356@icloud.com>
2023-07-14 19:32:53 -0500

    add mod files

---
+++ src/Patches/NetPackageSetBlock.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using ProtectHighTierPrefabs.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace ProtectHighTierPrefabs.Patches
+{
+    [HarmonyPatch(typeof(NetPackageSetBlock), "ProcessPackage")]
+    internal class NetPackageSetBlock_ProcessPackage_Patches
+    {
+        private static readonly ModLog<NetPackageSetBlock_ProcessPackage_Patches> _log = new ModLog<NetPackageSetBlock_ProcessPackage_Patches>();
+
+        public static bool Prefix(GameManager _callbacks, int ___localPlayerThatChanged, PlatformUserIdentifierAbs ___persistentPlayerId, ref List<BlockChangeInfo> ___blockChanges)
+        {
+            try
+            {
+                BlockPlacementManager.HandleBlockChanges(_callbacks, ___localPlayerThatChanged, ___persistentPlayerId, ___blockChanges, out ___blockChanges);
+
+                // only allow further processing and broadcast to other players if there are any block changes not already reversed
+                return ___blockChanges.Count > 0;
+            }
+            catch (Exception e)
+            {
+                _log.Error($"Failed to handle prefix for NetPackageSetBlock.ProcessPackage on behalf of ___persistentPlayerId: {___persistentPlayerId} / ___localPlayerThatChanged: {___localPlayerThatChanged}", e);
+                return true;
+            }
+        }
+    }
+}
---
+++ src/Utilities/PlayerHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ProtectHighTierPrefabs.Utilities
+{
+    internal class PlayerHelper
+    {
+        public static bool TryGetClientInfo(int entityId, out ClientInfo clientInfo)
+        {
+            clientInfo = ConnectionManager.Instance.Clients.ForEntityId(entityId);
+            return clientInfo != null;
+        }
+    }
+}
